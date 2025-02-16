2e5f1ba1c378f5bd3f3f2f48867745dd4a971c6b
Kartel Maksym <kartmax1804@gmail.com>
2025-02-16 10:29:21 +0100

    fix max-width toast

--- src/BaseToast.jsx
+++ src/BaseToast.jsx
@@ -1,6 +1,6 @@
 function BaseToast () {
     return (
-        <div className="fixed left-1/2 -translate-x-1/2 z-30 bottom-28 bg-[#2e76d0] text-white text-center tracking-wide whitespace-nowrap rounded-lg py-3 px-8 pointer-events-none">
+        <div className="max-w-[90vw] fixed left-1/2 -translate-x-1/2 z-30 bottom-28 bg-[#2e76d0] text-white text-center tracking-wide whitespace-nowrap rounded-lg py-3 px-8 pointer-events-none">
             Link copied to clipboard
         </div>
     )
