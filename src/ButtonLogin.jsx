cab7b1254439666cbba9e939a4a75ff584b43425
Kartel Maksym <kartmax1804@gmail.com>
2025-02-16 13:49:21 +0100

    BasePopover - styles

--- src/ButtonLogin.jsx
+++ src/ButtonLogin.jsx
@@ -1,6 +1,6 @@
 function ButtonLogin() {
     return (
-        <a href="/" className="bg-white text-[#2e2e2e] text-sm sm:text-[16px] font-semibold leading-4 tracking-widest px-[16px] sm:px-[38px] py-[10px] sm:py-[16px] rounded-full hover:scale-105 duration-200">Login</a>
+        <a href="/" className="inline-block bg-white text-[#2e2e2e] text-sm sm:text-[16px] font-semibold leading-4 tracking-widest px-[16px] sm:px-[38px] py-[10px] sm:py-[16px] rounded-full hover:scale-105 duration-200">Login</a>
     )
 }
 
--- src/ButtonSignUp.jsx
+++ src/ButtonSignUp.jsx
@@ -1,6 +1,6 @@
 function ButtonSignUp() {
     return (
-        <a href="/" className="text-[#969696] text-sm sm:text-[16px] font-semibold leading-4 tracking-widest px-[12px] sm:px-[25px] py-[10px] hover:scale-105 hover:text-white duration-200">Sign Up</a>
+        <a href="/" className="inline-block text-[#969696] text-sm sm:text-[16px] font-semibold leading-4 tracking-widest px-[12px] sm:px-[25px] py-[10px] hover:scale-105 hover:text-white duration-200">Sign Up</a>
     )
 }
 
